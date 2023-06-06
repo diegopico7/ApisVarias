887b5f2a860098ae1b0a9801a621367903e1cf72
diegopico7 <dgpicasso@hotmail.com>
2023-06-06 05:32:38 +0200

    completando tienda

--- tienda/tiendas.js
+++ tienda/tiendas.js
@@ -76,20 +76,84 @@ function showFetch1(){
     }
     
     showFetch1();
+    
 
     function AllProductos1(data) {
         console.log(data)
+        
         const tarjetasList = data.map(d =>{
            console.log(d.title)
            const div = document.getElementById("card1")
            const h2 = document.createElement('h2')
-           const pri = document.createElement('p')
+           const im = document.createElement('img')
+           const desc = document.createElement('p')
+           const pri = document.createElement('h3')
+           const cat = document.createElement('h5')
+
+h2.classList.add('tit')
+im.classList.add('img')
+desc.classList.add('desc')
+cat.classList.add('cat')
+pri.classList.add('pri')
+
+
            h2.textContent = d.title
-        pri.textContent = d.price
+           im.src=d.image
+           desc.innerHTML = d.description
+           cat.innerHTML = d.category
+           
+        pri.textContent = `$ ${d.price}`
+        im.innerHTML = d.image
        div.appendChild(h2)
        div.appendChild(pri) 
+       div.appendChild(im)
+       div.appendChild(desc)
+       div.appendChild(cat)
 
         })
         tarjetasList;
+
+               
+document.getElementById("btnAgregar").addEventListener("click", ()=>{
+    fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: 'Producto Nuevo',
+                    price: 158,
+                    description: 'Producto importado recien llegado',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            // .then(res=>res.json())
+            // .then(json=>console.log(json))
+})
      
     }
+
+    document.getElementById("btnEditar").addEventListener("click", ()=>{
+        fetch('https://fakestoreapi.com/products/1',{
+            method:"PUT",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+    })
+    
+document.getElementById("btnBorrar").addEventListener("click", ()=>{
+    fetch('https://fakestoreapi.com/products/1',{
+            method:"DELETE"
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+})
